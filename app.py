968e779ff5e8672c5259e57ad46ec28b6b997dbc
yumsha <rizagapakel30@gmail.com>
2025-08-22 14:33:27 +0700

    Initial Commit

---
+++ app.py
@@ -0,0 +1,60 @@
+import os
+import requests
+import base64
+import schedule
+import time
+from dotenv import load_dotenv
+from datetime import datetime
+
+
+load_dotenv()
+
+ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
+REPO_NAME = os.getenv("REPO_NAME")
+FILE_NAME = os.getenv("FILE_NAME", "README.md")
+
+# API URL GitHub
+API_URL = f"https://api.github.com/repos/{REPO_NAME}/contents/{FILE_NAME}"
+
+def auto_commit():
+    print(f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] 🔄 Running auto commit...")
+
+    res = requests.get(API_URL, headers={
+        "Authorization": f"token {ACCESS_TOKEN}"
+    })
+    
+    if res.status_code != 200:
+        print("Couldn't get a files:", res.json())
+        return
+    
+    data = res.json()
+    sha = data["sha"]
+
+
+    content = base64.b64decode(data["content"]).decode()
+
+    new_line = f"\nUpdate at {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}"
+    updated_content = content + new_line
+
+
+    payload = {
+        "message": f"chore: auto update {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}",
+        "content": base64.b64encode(updated_content.encode()).decode(),
+        "sha": sha
+    }
+
+    push_res = requests.put(API_URL, headers={
+        "Authorization": f"token {ACCESS_TOKEN}"
+    }, json=payload)
+
+    if push_res.status_code in [200, 201]:
+        print("Commit Succeded")
+    else:
+        print("Try again!", push_res.json())
+
+# 3 hours repeat
+schedule.every(10).hour.do(auto_commit)
+print("Auto commit service started! Interval: 1 minute")
+while True:
+    schedule.run_pending()
+    time.sleep(60)
